482b85566befdaf3f14d88968b7308c19dddd381
george.vlad <geo_vlad@yahoo.com>
2026-03-13 02:07:47 +0200

    Render debates page from runtime DB

--- src/app/debates/page.tsx
+++ src/app/debates/page.tsx
@@ -1,6 +1,8 @@
 import { getPhilosophersMap, getAllPhilosophers, getAllDebates } from "@/lib/data";
 import { DebatesPageClient } from "./DebatesPageClient";
 
+export const dynamic = "force-dynamic";
+
 export default function DebatesListPage() {
   const philosophersMap = getPhilosophersMap();
   const philosophers = getAllPhilosophers();
